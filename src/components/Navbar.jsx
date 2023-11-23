03f1f3c89525575017ae51d428ae51fd461ca83d
abdelrahman adel <abdelrahman20191700364@cis.asu.edu.eg>
2023-11-23 21:43:53 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Nav() {
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"} px={"3rem"}>
           <Box textTransform={"capitalize"} fontWeight={"bolder"}>
             shooping website
           </Box>
